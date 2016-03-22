81fab80ddc29caa9e27419f03bd0b565ace1f74e
Jordan Ell <jordan.ell7@gmail.com>
2016-03-21 21:12:05 -0700

    Added google analytics.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+GOOGLE_ANALYTICS = ENV['DEVELOPMENT_GOOGLE_ANALYTICS']
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+GOOGLE_ANALYTICS = ENV['TEST_GOOGLE_ANALYTICS']
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
